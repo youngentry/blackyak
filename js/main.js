23fc9a2306e66172dd70dac4d599ca49bc3e105b
youngentry <gentry_@naver.com>
2022-07-12 17:50:33 +0900

    220712

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
-$(function () {});
+$(function () {
+  $("html, body").on("mousewheel", function (e) {
+    var height = $(document).scrollTop();
+    var d = e.originalEvent.deltaY;
+    if (height >= 700) {
+      $(".header").css("background", "RGB(208, 203, 197)").css("border", "1px solid #999");
+      if (d > 0) {
+        $(".header").addClass("on");
+        // console.log("내림");
+      } else if (d < 0) {
+        $(".header").removeClass("on");
+
+        // console.log("올림");
+      }
+    }
+  });
+  //   $(window).scroll(function (e) {
+  //     console.log(e);
+  //   var height = $(document).scrollTop();
+  //   if (height >= 700) {
+  //     $(".header").addClass("on");
+  //   } else {
+  //     $(".header").removeClass("on");
+  //   }
+  //   });
+
+  $(".productSlider").slick({
+    arrows: false,
+    slidesToShow: 2,
+    variableWidth: true,
+  });
+});
