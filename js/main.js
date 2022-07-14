426874fe434e21cd5098cdd8868d1622bd00c5c5
youngentry <gentry_@naver.com>
2022-07-14 17:51:03 +0900

    220714

--- js/main.js
+++ js/main.js
@@ -1,33 +1,59 @@
 $(function () {
   $("html, body").on("mousewheel", function (e) {
-    var height = $(document).scrollTop();
+    var sct = $(document).scrollTop();
+    var innovation = $(".innovationWrap").offset();
+    var stickyBoxWall = $(".box-wall").offset();
     var d = e.originalEvent.deltaY;
-    if (height >= 600) {
+    if ((sct >= 600 && sct < innovation.top - 200) || sct > stickyBoxWall.top - 500) {
       $(".header").css("background", "#f2f2f2").css("border-bottom", "1px solid #999");
       if (d > 0) {
         $(".header").addClass("on"); // DOWN!!!
       }
-    } else {
-      $(".header").css("background", "transparent").css("border-bottom", "none");
       if (d < 0) {
         $(".header").removeClass("on"); // UP!!!
       }
+    } else {
+      $(".header").css("background", "transparent").css("border-bottom", "none");
+      if (d > 0) {
+        $(".header").addClass("on"); // DOWN!!!
+      }
     }
   });
-  //   $(window).scroll(function (e) {
-  //     console.log(e);
-  //   var height = $(document).scrollTop();
-  //   if (height >= 700) {
-  //     $(".header").addClass("on");
-  //   } else {
-  //     $(".header").removeClass("on");
-  //   }
-  //   });
+
+  $(window).on("scroll", function (e) {
+    var heritage = $(".heritage").offset();
+    var sct = $(document).scrollTop();
+    var introHead = $("#introduction p, #introduction .head");
+    var stickyFlowTop = heritage.top - 2000;
+    if (sct > stickyFlowTop) {
+      introHead.css("visibility", "hidden");
+    } else if (sct < stickyFlowTop) {
+      introHead.css("visibility", "visible");
+    }
+  });
 
   $(".productSlider").slick({
     arrows: false,
     slidesToShow: 2,
     variableWidth: true,
     touchThreshold: 25,
+  });
+  $(".seasonSlider").slick({
+    autoplay: true,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+    arrows: false,
+    slidesToShow: 4,
+    touchThreshold: 25,
+    swipeToSlide: true,
+  });
+  $(".promotionSlider").slick({
+    autoplay: true,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+    arrows: true,
+    slidesToShow: 1,
+    touchThreshold: 25,
+    swipeToSlide: true,
   });
 });
